d2a99e509de113b4446af017228c707a2625a1ae
Brent Beachtel <158010061+bbeachtel@users.noreply.github.com>
2024-05-21 20:13:32 -0500

    fetched data, created GeoJSON objects for each feature.

---
+++ static/js/logic.js
@@ -0,0 +1,35 @@
+// Store our API endpoint as queryUrl.
+let queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+
+// Perform a GET request to the query URL/
+d3.json(queryUrl).then(function (data) {
+    // Once we get a response, send the data.features object to the createFeatures function.
+    createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+    // // Define a function that we want to run once for each feature in the features array.
+    // // Give each feature a popup that describes the place and time of the earthquake.
+    function onEachFeatureFn(feature, layer) {
+        layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+    }
+
+    // Create a GeoJSON layer that contains the features array on the earthquakeData object.
+    // Run the onEachFeature function once for each piece of data in the array.
+    let earthquakes = L.geoJSON(earthquakeData, {
+        onEachFeature: onEachFeatureFn
+    });
+
+    // let earthquakes = L.geoJSON(earthquakeData, {
+    //   'pointToLayer': function(geoJsonPoint, latlng){
+    //                     var circleMarker=L.circle(latlng, {
+    //                       'radius': geoJsonPoint['properties']['mag']*100000
+    //                     });
+    //                     return circleMarker
+    //   }
+    // });
+
+    // Send our earthquakes layer to the createMap function/
+    createMap(earthquakes);
+}
